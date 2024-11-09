6ab725ba5cebaaab388652cda922168233124ff2
Takashi-Matsumura <mats@coral.plala.or.jp>
2024-11-09 14:43:56 +0900

    ページヘッダーコンポーネントを追加し、ホームページに埋め込みスクリプトを動的に読み込む機能を実装

---
+++ app/components/PageHeader.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PageHeader = () => {
+  return (
+    <div className="fixed top-0 left-0 w-full flex items-center px-4 py-2 shadow-lg bg-white z-50">
+      <div>Header</div>
+    </div>
+  );
+};
+
+export default PageHeader;
